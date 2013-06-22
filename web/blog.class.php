4b6cc7726791852261a4d8c9592da52656dd2a01
Victoria <gonchar.vik@mail.ru>
2013-06-22 21:05:40 +0400

    added print_post,modified get_post

--- web/blog.class.php
+++ web/blog.class.php
@@ -1,12 +1,10 @@
 <?php
-
 class Blog{
 	public $db_server = 'localhost';
 	public $username = 'root';
 	public $password = '';
 	public $database = 'posts';
 	public $db_table = 'posts';
-	
 function db_connect(){
 	try {
 		$conn = new PDO('mysql:host=localhost;dbname=posts', $this->username, $this->password);
@@ -23,25 +21,40 @@ function get_all_posts(){
 	$conn = $this->db_connect();
 	$res = $conn ->prepare('SELECT title FROM posts ORDER BY created_at DESC');
 	$res ->execute();
-	$i=0;
+	$i = 0;
 	while($row = $res->fetch(PDO::FETCH_ASSOC)){
 		foreach($row as $key => $val){
 			$results[$i] = $val;
 		}
-	$i++;
+		$i++;
 	}
+		
 	return $results;
 }
 function print_all_posts(){
-	$results = $this ->get_all_posts();
-	foreach ($results as $key){
-		echo '<p>'.$key.'</p>';
+	$result = $this ->get_all_posts();
+	foreach ($result as $title){
+		echo '<h2>'.$title.'</h2>';
 	}
 }
 function get_post(){
 	$conn=$this->db_connect();
-	
+	$res = $conn->prepare('SELECT title, post, created_at FROM posts WHERE id=:id');
+	$res ->bindParam (':id',$_GET['id'], PDO::PARAM_INT);
+	$res ->execute();
+	$results = $res->fetch(PDO::FETCH_ASSOC);
+	return $results;
+}
+
+function print_post(){
+	if (isset($_GET['id'])){
+		$result=$this->get_post();
+		foreach ($result as $key){
+			echo '<p>'.$key.'</p>';
+		}
+	}
 }
 }
 $blog = new Blog;
 $blog ->print_all_posts();
+$blog ->print_post();
