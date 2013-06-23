e65a14e76aa77f70d26b572c9bb76450534e1054
Victoria <gonchar.vik@mail.ru>
2013-06-24 00:32:39 +0400

    blog commit

--- web/blog.class.php
+++ web/blog.class.php
@@ -1,37 +1,36 @@
 <?php
 class Blog{
-	public $db_server = 'localhost';
-	public $username = 'root';
-	public $password = '';
-	public $database = 'posts';
-	public $db_table = 'posts';
+	private $db_server = 'localhost';
+	private $username = 'root';
+	private $password = '';
+	private $database = 'posts';
 function db_connect(){
 	try {
-		$conn = new PDO('mysql:host=localhost;dbname=posts', $this->username, $this->password);
-		$conn->setAttribute(
+		$conn = new PDO("mysql:host=$this->db_server;dbname=$this->database", $this->username, $this->password);
+		$conn -> setAttribute(
 		PDO::ATTR_ERRMODE, 
 		PDO::ERRMODE_EXCEPTION);
 		return $conn;
 	}
 	catch(PDOException $e) {
-		echo 'ERROR: ' . $e->getMessage();
+		echo 'ERROR: ' . $e -> getMessage();
 	}	
 }
 function get_all_posts(){
-	$conn = $this->db_connect();
-	$res = $conn ->prepare('SELECT title FROM posts ORDER BY created_at DESC');
-	$res ->execute();
-	$index = 0;
-	while($row = $res->fetch(PDO::FETCH_ASSOC)){
+	$conn = $this-> db_connect();
+	$res = $conn -> prepare('SELECT title FROM posts ORDER BY created_at DESC');
+	$res -> execute();
+	$i = 0;
+	while($row = $res -> fetch(PDO::FETCH_ASSOC)){
 		foreach($row as $key => $val){
-			$results[$index] = $val;
+			$results[$i] = $val;
 		}
-		$index++;
-		}
+		$i++;
+	}
 	return $results;
 }
 function print_all_posts(){
-	$result = $this ->get_all_posts();
+	$result = $this -> get_all_posts();
 	foreach ($result as $title){
 		echo '<h2>'.$title.'</h2>';
 	}
@@ -39,8 +38,8 @@ function print_all_posts(){
 function print_post(){
 	$conn = $this -> db_connect();
 	$res = $conn -> prepare('SELECT title, post, created_at FROM posts WHERE id=:id');
-	$res ->execute(array(':id'=>$_GET['id']));
-	$results = $res->fetch(PDO::FETCH_ASSOC);
+	$res -> execute(array(':id'=>$_GET['id']));
+	$results = $res -> fetch(PDO::FETCH_ASSOC);
 	if ($results != null) {
 		foreach ($results as $key){
 			echo '<p>'.$key.'</p>';
@@ -52,31 +51,31 @@ function print_post(){
 	}
 }
 function add_post (){
-	$conn=$this->db_connect();
-	$res=$conn->prepare('INSERT INTO posts(title,post,created_at) VALUES (:title, :post, :created_at)' );
+	$conn = $this -> db_connect();
+	$res = $conn -> prepare('INSERT INTO posts(title,post,created_at) VALUES (:title, :post, :created_at)' );
 	$created_at = date(YmdHms);
-	$res ->execute(array(':title'=>$_POST['title'], ':post'=>$_POST['post'],':created_at'=>$created_at));
+	$res ->  execute(array(':title' => $_POST['title'], ':post' => $_POST['post'],':created_at' => $created_at));
 }
 
 function get_title(){
-	$conn=$this ->db_connect();
-	$res =$conn->prepare('SELECT title FROM posts WHERE id=:id');
-	$res ->execute(array(':id'=>$_GET['id']));
-		for($i=0; $row = $res->fetch(); $i++){
+	$conn = $this -> db_connect();
+	$res = $conn -> prepare('SELECT title FROM posts WHERE id=:id');
+	$res -> execute(array(':id' => $_GET['id']));
+		for($i=0; $row = $res -> fetch(); $i++){
 			echo $row['title'];
 		}
 }
 function get_text(){
-	$conn=$this ->db_connect();
-	$res =$conn->prepare('SELECT post FROM posts WHERE id=:id');
-	$res ->execute(array(':id'=>$_GET['id']));
-	for($i=0; $row = $res->fetch(); $i++){
+	$conn = $this -> db_connect();
+	$res = $conn -> prepare('SELECT post FROM posts WHERE id=:id');
+	$res -> execute(array(':id'=>$_GET['id']));
+	for($i=0; $row = $res -> fetch(); $i++){
         echo $row['post'];
     }
 }
 function update_post(){
-	$conn=$this->db_connect();
-	$res=$conn->prepare('UPDATE posts SET title=:title, post=:post WHERE id=:id');
-	$res ->execute(array(':id'=>$_GET['id'],':title'=>$_POST['title'],':post'=>$_POST['post']));
+	$conn = $this -> db_connect();
+	$res = $conn -> prepare('UPDATE posts SET title=:title, post=:post WHERE id=:id');
+	$res -> execute(array(':id'=>$_GET['id'],':title'=>$_POST['title'],':post'=>$_POST['post']));
 	}
 }
